3b60263a1dea13aec7de91d66e4c804ad8de177d
tejasvp25 <tejasvp25@gmail.com>
2022-01-03 07:14:11 +0000

    NavigationModeController: Silence log spam
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I5f2f76039280ccef5e6ed360d1e45cce759c21fa

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationModeController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationModeController.java
@@ -143,7 +143,6 @@ public class NavigationModeController implements Dumpable {
                     Secure.NAVIGATION_MODE, String.valueOf(mode)));
         if (DEBUG) {
             Log.e(TAG, "updateCurrentInteractionMode: mode=" + mode);
-            dumpAssetPaths(mCurrentUserContext);
         }
 
         if (notify) {
@@ -233,15 +232,5 @@ public class NavigationModeController implements Dumpable {
             defaultOverlays = "failed_to_fetch";
         }
         pw.println("  defaultOverlays=" + defaultOverlays);
-        dumpAssetPaths(mCurrentUserContext);
-    }
-
-    private void dumpAssetPaths(Context context) {
-        Log.d(TAG, "  contextUser=" + mCurrentUserContext.getUserId());
-        Log.d(TAG, "  assetPaths=");
-        ApkAssets[] assets = context.getResources().getAssets().getApkAssets();
-        for (ApkAssets a : assets) {
-            Log.d(TAG, "    " + a.getAssetPath());
-        }
     }
 }
